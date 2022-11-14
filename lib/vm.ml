93cff97f37045830b53d9a3f425faec0575fe614
Robert Peterson <me@robertp.me>
2022-11-14 11:19:22 -0800

    bools and cleanup

--- lib/vm.ml
+++ lib/vm.ml
@@ -1,30 +1,44 @@
 type stack_obj =
+  | SO_BOOL of bool
   | SO_INT of int
 
 type stack = stack_obj list
 
 type opcode =
   | OP_PUSH of stack_obj
+  | OP_AND
   | OP_ADD
   | OP_PRINT
 
-type vm_error = StackSize
+type vm_error = StackSize | ArgumentType
 
 exception VM_ERROR of vm_error
 
 let string_of_stack_obj = function
+  | SO_BOOL b -> string_of_bool b
   | SO_INT i -> string_of_int i
 
 let print_obj obj =
   print_endline (string_of_stack_obj obj)
 
-let add left right =
+let and_op left right =
+  match left, right with
+  | SO_BOOL a, SO_BOOL b -> SO_BOOL (a && b)
+  | _ -> raise (VM_ERROR ArgumentType)
+
+let add_op left right =
   match left, right with
   | SO_INT a, SO_INT b -> SO_INT (a + b)
+  | _ -> raise (VM_ERROR ArgumentType)
 
 let pop = function
   | head :: stack -> (head, stack)
   | _ -> raise (VM_ERROR StackSize)
+
+let pop2 stack =
+  let (a, s)  = pop stack in
+  let (b, s') = pop s in
+  (a, b, s')
 
 let run_program program =
   let len = Array.length(program) in
@@ -33,10 +47,12 @@ let run_program program =
       match program.(pc) with
       | OP_PUSH obj ->
           loop (pc + 1) (obj :: stack)
+      | OP_AND ->
+          let (l, r, s) = pop2 stack in
+          loop (pc + 1) (and_op l r :: s)
       | OP_ADD ->
-          let (l, s)  = pop stack in
-          let (r, s') = pop s in
-          loop (pc + 1) (add l r :: s')
+          let (l, r, s) = pop2 stack in
+          loop (pc + 1) (add_op l r :: s)
       | OP_PRINT ->
           let (obj, s) = pop stack in
           print_obj obj;
