fb07e96c56921d3b05e821d3422d9ac266d3a5d9
Robert Peterson <me@robertp.me>
2022-11-14 23:02:50 -0800

    basic program

--- lib/vm.ml
+++ lib/vm.ml
@@ -1,63 +1,36 @@
-type stack_obj =
-  | SO_BOOL of bool
-  | SO_INT of int
-
-type stack = stack_obj list
+type value =
+  | VInt of int
+  | VBool of bool
+  | VString of string
 
 type opcode =
-  | OP_PUSH of stack_obj
-  | OP_PRINT
-  | OP_AND
-  | OP_ADD
+  | OPush of value
+  | OPop
+  | OPrint
 
-type vm_error = StackSize | ArgumentType
+type program = opcode array
 
-exception VM_ERROR of vm_error
+exception VmError of string
 
-let string_of_stack_obj = function
-  | SO_BOOL b -> string_of_bool b
-  | SO_INT i -> string_of_int i
-
-let print_op obj =
-  print_endline (string_of_stack_obj obj)
-
-let and_op left right =
-  match left, right with
-  | SO_BOOL a, SO_BOOL b -> SO_BOOL (a && b)
-  | _ -> raise (VM_ERROR ArgumentType)
-
-let add_op left right =
-  match left, right with
-  | SO_INT a, SO_INT b -> SO_INT (a + b)
-  | _ -> raise (VM_ERROR ArgumentType)
+let string_of_value = function
+  | VInt i -> string_of_int i
+  | VBool b -> string_of_bool b
+  | VString s -> s
 
 let pop = function
-  | head :: stack -> (head, stack)
-  | _ -> raise (VM_ERROR StackSize)
-
-let pop2 stack =
-  let (a, s)  = pop stack in
-  let (b, s') = pop s in
-  (a, b, s')
+  | h :: stack -> (h, stack)
+  | _ -> raise (VmError "stack size too small")
 
 let run_program program =
   let len = Array.length(program) in
   let rec loop pc stack =
-    if pc < len then
-      match program.(pc) with
-      | OP_PUSH obj ->
-          loop (pc + 1) (obj :: stack)
-      | OP_AND ->
-          let (l, r, s) = pop2 stack in
-          loop (pc + 1) (and_op l r :: s)
-      | OP_ADD ->
-          let (l, r, s) = pop2 stack in
-          loop (pc + 1) (add_op l r :: s)
-      | OP_PRINT ->
-          let (obj, s) = pop stack in
-          print_op obj;
-          loop (pc + 1) s
-    else
-      ()
+    if pc >= len then () else
+    match program.(pc) with
+    | OPush v -> loop (pc + 1) (v :: stack)
+    | OPop -> let _, s = pop stack in loop (pc + 1) s
+    | OPrint ->
+        let v, s = pop stack in
+        print_string (string_of_value v);
+        loop (pc + 1) s
   in
   loop 0 []
