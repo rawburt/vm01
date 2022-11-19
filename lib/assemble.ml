109af4abe419d64a95a8660f9e073d67fb328334
Robert Peterson <me@robertp.me>
2022-11-19 14:14:02 -0800

    replace labels

--- lib/assemble.ml
+++ lib/assemble.ml
@@ -8,4 +8,26 @@ let extract_labels ast =
         loop (i + 1) labels (instruction :: left) rest
     | [] -> (labels, left)
   in
-  loop 0 [] [] ast
+  let labels, ast' = loop 0 [] [] ast in
+  (labels, List.rev ast')
+
+let rec find_label name = function
+  | (n, i) :: rest ->
+      if n = name
+      then Some (n, i)
+      else find_label name rest
+  | [] -> None
+
+exception Assemble_error of string
+
+let rec replace_labels labels = function
+  | [] -> []
+  | P.Label _ :: _ -> raise (Assemble_error "unexpected label")
+  | P.Instruction (ins, Some (P.ArgLabel name)) :: rest -> begin
+      match find_label name labels with
+      | None -> raise (Assemble_error ("unknown label: " ^ name))
+      | Some (_, id) ->
+          let instruction = P.Instruction (ins, Some (P.ArgInt id)) in
+          instruction :: (replace_labels labels rest)
+  end
+  | (_ as head) :: rest -> head :: (replace_labels labels rest)
